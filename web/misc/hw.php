3709d6f02c9bf5c93150c030b567165debbbf419
Jitesh <46092054+jiteshsaini@users.noreply.github.com>
2021-06-02 12:58:50 +0530

    Update hw.php

--- web/misc/hw.php
+++ web/misc/hw.php
@@ -4,6 +4,6 @@
 $entry_by=$_POST["entry_by"];
 $page=$_POST["page"];
 
-$xx=exec("sudo python /var/www/html/earthrover/control_panel/misc/hw.py $entry_by $page &");
+$xx=exec("sudo python /var/www/html/model_garden/web/misc/hw.py $entry_by $page &");
 
 ?>
